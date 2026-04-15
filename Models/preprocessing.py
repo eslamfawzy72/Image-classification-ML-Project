cd0aea7562e54a24aa7af2e5168cffd95702fa7a
MohamedBadra <MohamedWBadra@gmail.com>
2026-04-15 18:00:04 +0200

    adding hog

--- Models/preprocessing.py
+++ Models/preprocessing.py
@@ -42,6 +42,94 @@ def get_class_weights(y):
     
     return {0: weights[0], 1: weights[1]}
 
+class CustomHOG:
+    
+    def __init__(self, pixels_per_cell=(7, 7), cells_per_block=(2, 2), num_bins=9):
+        self.pixels_per_cell = pixels_per_cell
+        self.cells_per_block = cells_per_block
+        self.num_bins = num_bins
+
+    def compute_gradients(self,image):
+    
+        padded_img = np.pad(image, pad_width=1, mode='constant', constant_values=0)
+    
+        Gx = padded_img[1:-1, 2:] - padded_img[1:-1, :-2]
+        
+    
+        Gy = padded_img[2:, 1:-1] - padded_img[:-2, 1:-1]
+        
+        magnitude = np.sqrt(Gx**2 + Gy**2)
+        
+        
+        angle_radians = np.arctan2(Gy, Gx)
+        angle_degrees = np.rad2deg(angle_radians)
+    
+        angle_unsigned = angle_degrees % 180
+        
+        return magnitude, angle_unsigned
+
+
+    def compute_cell_histograms(self,magnitude, angle):
+        
+        h, w = magnitude.shape
+        c_h, c_w = self.pixels_per_cell
+        cells_y = h // c_h
+        cells_x = w // c_w
+        
+        histograms = np.zeros((cells_y, cells_x,self. num_bins))
+        
+        for y in range(cells_y):
+            for x in range(cells_x):
+                y_start = y * c_h
+                y_end = y_start + c_h
+                x_start = x * c_w
+                x_end = x_start + c_w
+                
+                cell_magnitude = magnitude[y_start:y_end, x_start:x_end]
+                cell_angle = angle[y_start:y_end, x_start:x_end]
+            
+                hist, _ = np.histogram(
+                    cell_angle.flatten(), 
+                    bins=self.num_bins, 
+                    range=(0, 180), 
+                    weights=cell_magnitude.flatten()
+                )
+                
+                histograms[y, x, :] = hist
+                
+        return histograms
+    
+
+    def normalize_blocks(self, histograms):
+        cells_y, cells_x, _ = histograms.shape
+        b_y, b_x = self.cells_per_block
+        
+        blocks_y = cells_y - b_y + 1
+        blocks_x = cells_x - b_x + 1
+        normalized_blocks = []
+        
+        for y in range(blocks_y):
+            for x in range(blocks_x):
+                block = histograms[y:y+b_y, x:x+b_x, :]
+                block_vector = block.flatten()
+                
+                epsilon = 1e-5 
+                norm = np.sqrt(np.sum(block_vector**2) + epsilon)
+                
+                normalized_blocks.append(block_vector / norm)
+                
+        return np.concatenate(normalized_blocks)
+
+    def extract_single(self, image):
+        mag, ang = self.compute_gradients(image)
+        hists = self.compute_cell_histograms(mag, ang)
+        feature_vector = self.normalize_blocks(hists)
+        return feature_vector
+
+    def transform(self, X_images):        
+        features = [self.extract_single(img) for img in X_images]
+        return np.array(features)
+
 
 def preprocess(feature_method="flatten", n_pca=50):
     print("Loading MNIST dataset...")
@@ -85,5 +173,14 @@ def preprocess(feature_method="flatten", n_pca=50):
         X_train_final = X_train.reshape(-1, 784)
         X_val_final = X_val.reshape(-1, 784)
         X_test_final = X_test.reshape(-1, 784)
+    
+    elif feature_method == "hog":
+        hog = CustomHOG()
+        X_train_final = hog.transform(X_train)
+        X_val_final = hog.transform(X_val)
+        X_test_final = hog.transform(X_test)
+        
+    else:
+        raise ValueError("Invalid feature_method. Choose 'flatten', 'pca', or 'hog'.")
 
     return X_train_final, y_train, X_val_final, y_val, X_test_final, y_test, weights
